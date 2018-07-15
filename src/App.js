87abfc48e897adef120d3379fdab5dc4588858eb
Aniruddha Shevle <aniruddhashevle@gmail.com>
2018-07-15 19:38:49 +0530

    Google Suggestion API is called with allowing CORS for anywhere with any origin API with styling changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchValue: ''
+      searchValue: '',
+      suggestionList: []
     }
   }
 
   onSearch = async (e) => {
     const { value } = e.target;
     this.setState({ searchValue: value });
+    if (value) {
+      try {
+        let corsAnywhere = 'https://cors-anywhere.herokuapp.com/', //to allow CORS for anywhere with any origin
+          response = await axios(`${corsAnywhere}https://suggestqueries.google.com/complete/search?client=firefox&hl=e&q=${value}`);
+        if (response && response.status === 200 && response.data && response.data.length > 0) {
+          this.setState({ suggestionList: response.data[1] })
+        }
+      } catch (error) {
+        //error handling
+      }
+    }
   }
 
   render() {
@@ -20,15 +33,19 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">React Search AutoCompletion</h1>
         </header>
-        <label>
-          Search Your Queries: 
-          <input
-            type="search"
-            name="search-query"
-            onChange={this.onSearch}
-            value={this.state.searchValue}
-          />
-        </label>
+        <main>
+          <div className="search-wrapper">
+            <label className="search-label" htmlFor="search-query">Search Your Queries:</label>
+            <input
+              type="search"
+              name="search-query"
+              id="search-query"
+              className="search-input"
+              onChange={this.onSearch}
+              value={this.state.searchValue}
+            />
+          </div>
+        </main>
       </div>
     );
   }
